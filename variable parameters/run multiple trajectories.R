8377dadfa507c3d752589576911b5e4f4f87d4c0
Luz Valerie Pascal <n11003324@qut.edu.au>
2024-08-22 14:45:43 +1000

    run multiple trajectories separate success and failure

--- variable parameters/run multiple trajectories.R
+++ variable parameters/run multiple trajectories.R
@@ -9,7 +9,7 @@ source("variable parameters/simulations.R")
 alphas <- read_policyx2(OUTPUT_FILE)
 
 # Define the function to run a single simulation
-run_simulation <- function(i) {
+run_simulation_failure <- function(i) {
   trajectory(
     state_prior_eco = tuple_to_index(1, N_ecosystem/2 + 1, N_ecosystem + 1),
     state_prior_tech = 1,
@@ -20,7 +20,26 @@ run_simulation <- function(i) {
     transition_tech = TR_FUNCTION_TECH,
     reward = REW,
     true_model = TRUE_MODEL,
-    true_model_tech = sample(1:2,1),
+    true_model_tech = 2,
+    alpha_momdp = alphas,
+    disc = GAMMA,
+    optimal_policy = TRUE,
+    naive_policy = NA,
+    alpha_indexes = FALSE
+  )$data_output[-86,]
+}
+run_simulation_success <- function(i) {
+  trajectory(
+    state_prior_eco = tuple_to_index(1, N_ecosystem/2 + 1, N_ecosystem + 1),
+    state_prior_tech = 1,
+    Tmax = 85,
+    initial_belief_state = B_PAR_TEST,
+    initial_belief_state_tech = B_PAR_TECH,
+    transition_ecosystem = TR_FUNCTION_ECO,
+    transition_tech = TR_FUNCTION_TECH,
+    reward = REW,
+    true_model = TRUE_MODEL,
+    true_model_tech = 1,
     alpha_momdp = alphas,
     disc = GAMMA,
     optimal_policy = TRUE,
@@ -54,28 +73,39 @@ for (true_scenario  in seq(length(TR_FUNCTION_ECO))){
     ))
 
     # Run the simulations in parallel
-    results <- parLapply(cl, 1:1000, run_simulation)
+    results_failure <- parLapply(cl, 1:500, run_simulation_failure)
+    results_success <- parLapply(cl, 1:500, run_simulation_success)
 
     # Stop the cluster
     stopCluster(cl)
 
     # Combine the results if needed
     # For example, if results are lists, you can combine them with do.call(rbind, results)
-    results <- bind_rows(results, .id = "sim")
+    results_failure <- bind_rows(results_failure, .id = "sim")
+    results_failure$tech_model <- 2
+    results_success <- bind_rows(results_success, .id = "sim")
+    results_success$tech_model <- 1
 
+    results <- rbind(results_success,
+                     results_failure)
     summ_results <- results %>%
       rowwise() %>%
       mutate(state_year=index_to_year(state_eco, N_ecosystem+1),
              state_ecosystem=index_to_eco(state_eco,N_ecosystem+1))%>%
       mutate(state_ecosystem=ecosystem_states[state_ecosystem],
-             action=action-1)%>%
-      group_by(time)%>%
+             action_dev=ifelse(action==1, 0,
+                               ifelse(state_tech==1, 1,0)),
+             action_deploy=ifelse(action==1, 0,
+                               ifelse(state_tech==2, 1,0)))%>%
+      group_by(time, tech_model)%>%
       summarise(mean_ecosystem = mean(state_ecosystem),
                 sd_ecosystem = sd(state_ecosystem),
-                mean_action=mean(action),
-                sd_action=sd(action),
-                upper_action=min(mean_action+sd_action, 1),
-                lower_action=max(mean_action-sd_action, 0),
+                mean_tech = mean(state_tech),
+                sd_tech = sd(state_tech),
+                mean_action_dev=mean(action_dev),
+                sd_action_dev=sd(action_dev),
+                mean_action_deploy=mean(action_deploy),
+                sd_action_deploy=sd(action_deploy),
                 mean_value=mean(value),
                 sd_value=sd(value)
       )
